38f05e2d32cb5f94f581c5347766f51981825a76
hpetalss <110082419+HPatel1210@users.noreply.github.com>
2023-06-08 11:56:25 +0530

    Add files via upload

---
+++ test/results/resultAlgorithm.js
@@ -0,0 +1,46 @@
+var q1 = localStorage.getItem("q1");
+var q2 = localStorage.getItem("q2");
+var q3 = localStorage.getItem("q3");
+var q4 = localStorage.getItem("q4");
+var q5 = localStorage.getItem("q5");
+var q6 = localStorage.getItem("q6");
+var q7 = localStorage.getItem("q7");
+var q8 = localStorage.getItem("q8");
+var q9 = localStorage.getItem("q9");
+var q10 = localStorage.getItem("q10");
+var q11 = localStorage.getItem("q11");
+var q12 = localStorage.getItem("q12");
+var q13 = localStorage.getItem("q13");
+var q14 = localStorage.getItem("q14");
+var q15 = localStorage.getItem("q15");
+var q16 = localStorage.getItem("q16");
+var q17 = localStorage.getItem("q17");
+
+function resultAlgorithm() {
+    q1 = parseInt(localStorage.getItem("q1"));
+    q2 = parseInt(localStorage.getItem("q2"));
+    q3 = parseInt(localStorage.getItem("q3"));
+    q4 = parseInt(localStorage.getItem("q4"));
+    q5 = parseInt(localStorage.getItem("q5"));
+    q6 = parseInt(localStorage.getItem("q6"));
+    q7 = parseInt(localStorage.getItem("q7"));
+    q8 = parseInt(localStorage.getItem("q8"));
+    q9 = parseInt(localStorage.getItem("q9"));
+    q10 = parseInt(localStorage.getItem("q10"));
+    q11 = parseInt(localStorage.getItem("q11"));
+    q12 = parseInt(localStorage.getItem("q12"));
+    q13 = parseInt(localStorage.getItem("q13"));
+    q14 = parseInt(localStorage.getItem("q14"));
+    q15 = parseInt(localStorage.getItem("q15"));
+    q16 = parseInt(localStorage.getItem("q16"));
+    q17 = parseInt(localStorage.getItem("q17"));
+
+    var obtainedMarks = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10 + q11 + q12 + q13 + q14 + q15 + q16 + q17;
+    var maxMarks = 85;
+    var percentage = obtainedMarks / maxMarks * 100;
+    percentage = Math.round(percentage);
+    console.log(percentage);
+    document.getElementById("resultDisplayPercentage").innerHTML = percentage;
+    localStorage.setItem("attemptTime", Date());
+}
+
---
+++ test/script.js
@@ -0,0 +1,291 @@
+
+
+function seta1forq1() {
+    localStorage.setItem("q1", "1");
+}
+function seta2forq1() {
+    localStorage.setItem("q1", "2");
+}
+function seta3forq1() {
+    localStorage.setItem("q1", "3");
+}
+function seta4forq1() {
+    localStorage.setItem("q1", "4");
+}
+function seta5forq1() {
+    localStorage.setItem("q1", "5");
+}
+
+function seta1forq2() {
+    localStorage.setItem("q2", "1");
+}
+function seta2forq2() {
+    localStorage.setItem("q2", "2");
+}
+function seta3forq2() {
+    localStorage.setItem("q2", "3");
+}
+function seta4forq2() {
+    localStorage.setItem("q2", "4");
+}
+function seta5forq2() {
+    localStorage.setItem("q2", "5");
+}
+
+// Repeat the above pattern for q3 to q17
+
+function seta1forq3() {
+    localStorage.setItem("q3", "1");
+}
+function seta2forq3() {
+    localStorage.setItem("q3", "2");
+}
+function seta3forq3() {
+    localStorage.setItem("q3", "3");
+}
+function seta4forq3() {
+    localStorage.setItem("q3", "4");
+}
+function seta5forq3() {
+    localStorage.setItem("q3", "5");
+}
+
+// Functions for q4
+function seta1forq4() {
+    localStorage.setItem("q4", "1");
+}
+function seta2forq4() {
+    localStorage.setItem("q4", "2");
+}
+function seta3forq4() {
+    localStorage.setItem("q4", "3");
+}
+function seta4forq4() {
+    localStorage.setItem("q4", "4");
+}
+function seta5forq4() {
+    localStorage.setItem("q4", "5");
+}
+
+// Repeat the pattern for q5 to q10
+
+function seta1forq5() {
+    localStorage.setItem("q5", "1");
+}
+function seta2forq5() {
+    localStorage.setItem("q5", "2");
+}
+function seta3forq5() {
+    localStorage.setItem("q5", "3");
+}
+function seta4forq5() {
+    localStorage.setItem("q5", "4");
+}
+function seta5forq5() {
+    localStorage.setItem("q5", "5");
+}
+
+// Repeat the pattern for q6 to q10
+
+function seta1forq6() {
+    localStorage.setItem("q6", "1");
+}
+function seta2forq6() {
+    localStorage.setItem("q6", "2");
+}
+function seta3forq6() {
+    localStorage.setItem("q6", "3");
+}
+function seta4forq6() {
+    localStorage.setItem("q6", "4");
+}
+function seta5forq6() {
+    localStorage.setItem("q6", "5");
+}
+
+// Repeat the pattern for q7 to q10
+
+function seta1forq7() {
+    localStorage.setItem("q7", "1");
+}
+function seta2forq7() {
+    localStorage.setItem("q7", "2");
+}
+function seta3forq7() {
+    localStorage.setItem("q7", "3");
+}
+function seta4forq7() {
+    localStorage.setItem("q7", "4");
+}
+function seta5forq7() {
+    localStorage.setItem("q7", "5");
+}
+
+// Repeat the pattern for q8 to q10
+
+function seta1forq8() {
+    localStorage.setItem("q8", "1");
+}
+function seta2forq8() {
+    localStorage.setItem("q8", "2");
+}
+function seta3forq8() {
+    localStorage.setItem("q8", "3");
+}
+function seta4forq8() {
+    localStorage.setItem("q8", "4");
+}
+function seta5forq8() {
+    localStorage.setItem("q8", "5");
+}
+
+// Repeat the pattern for q9 to q10
+
+function seta1forq9() {
+    localStorage.setItem("q9", "1");
+}
+function seta2forq9() {
+    localStorage.setItem("q9", "2");
+}
+function seta3forq9() {
+    localStorage.setItem("q9", "3");
+}
+function seta4forq9() {
+    localStorage.setItem("q9", "4");
+}
+function seta5forq9() {
+    localStorage.setItem("q9", "5");
+}
+
+// Function for q10
+function seta1forq10() {
+    localStorage.setItem("q10", "1");
+}
+function seta2forq10() {
+    localStorage.setItem("q10", "2");
+}
+function seta3forq10() {
+    localStorage.setItem("q10", "3");
+}
+function seta4forq10() {
+    localStorage.setItem("q10", "4");
+}
+function seta5forq10() {
+    localStorage.setItem("q10", "5");
+}
+function seta1forq11() {
+    localStorage.setItem("q11", "1");
+}
+function seta2forq11() {
+    localStorage.setItem("q11", "2");
+}
+function seta3forq11() {
+    localStorage.setItem("q11", "3");
+}
+function seta4forq11() {
+    localStorage.setItem("q11", "4");
+}
+function seta5forq11() {
+    localStorage.setItem("q11", "5");
+}
+
+// Repeat the pattern for q12 to q16
+
+function seta1forq12() {
+    localStorage.setItem("q12", "1");
+}
+function seta2forq12() {
+    localStorage.setItem("q12", "2");
+}
+function seta3forq12() {
+    localStorage.setItem("q12", "3");
+}
+function seta4forq12() {
+    localStorage.setItem("q12", "4");
+}
+function seta5forq12() {
+    localStorage.setItem("q12", "5");
+}
+
+function seta1forq13() {
+    localStorage.setItem("q13", "1");
+}
+function seta2forq13() {
+    localStorage.setItem("q13", "2");
+}
+function seta3forq13() {
+    localStorage.setItem("q13", "3");
+}
+function seta4forq13() {
+    localStorage.setItem("q13", "4");
+}
+function seta5forq13() {
+    localStorage.setItem("q13", "5");
+}
+
+// Repeat the pattern for q14 to q16
+
+function seta1forq14() {
+    localStorage.setItem("q14", "1");
+}
+function seta2forq14() {
+    localStorage.setItem("q14", "2");
+}
+function seta3forq14() {
+    localStorage.setItem("q14", "3");
+}
+function seta4forq14() {
+    localStorage.setItem("q14", "4");
+}
+function seta5forq14() {
+    localStorage.setItem("q14", "5");
+}
+
+function seta1forq15() {
+    localStorage.setItem("q15", "1");
+}
+function seta2forq15() {
+    localStorage.setItem("q15", "2");
+}
+function seta3forq15() {
+    localStorage.setItem("q15", "3");
+}
+function seta4forq15() {
+    localStorage.setItem("q15", "4");
+}
+function seta5forq15() {
+    localStorage.setItem("q15", "5");
+}
+
+function seta1forq16() {
+    localStorage.setItem("q16", "1");
+}
+function seta2forq16() {
+    localStorage.setItem("q16", "2");
+}
+function seta3forq16() {
+    localStorage.setItem("q16", "3");
+}
+function seta4forq16() {
+    localStorage.setItem("q16", "4");
+}
+function seta5forq16() {
+    localStorage.setItem("q16", "5");
+}
+function seta1forq17() {
+    localStorage.setItem("q17", "1");
+}
+function seta2forq17() {
+    localStorage.setItem("q17", "2");
+}
+function seta3forq17() {
+    localStorage.setItem("q17", "3");
+}
+function seta4forq17() {
+    localStorage.setItem("q17", "4");
+}
+function seta5forq17() {
+    localStorage.setItem("q17", "5");
+}
+
+
